a1bf8de5fcc3c389ea88a8c9f3f40fa668d4d745
Adam Moss <adam.moss@nottingham.ac.uk>
2014-05-17 16:21:42 +0100

    Parameter file

--- inidriver.F90
+++ inidriver.F90
@@ -1,46 +1,48 @@
 !  Driving program
 
 program driver
- 
+  use IniFile
+  use AMLUtils
   use pk_rsd
   use timer
   implicit none
   character(LEN=8) :: elapsed_time
+  character(len=200) :: InputFile
   character(len=200) :: pkfile, regpt_dd,regpt_dt,regpt_tt
   character(len=200) :: outroot
 
+  logical bad
+
   call timer_start()
 
-  !pkfile = 'data/matterpower.dat'
-  !pkfile = 'camb/output/test_matterpower_z0.dat'
+  InputFile = ''
+  if (GetParamCount() /= 0)  InputFile = GetParam(1)
+  if (InputFile == '') stop 'No parameter input file'
 
-  pkfile = 'camb/output/test_matterpower_z057.dat'
-  regpt_dd = 'RegPT/pk_RegPT.dat_dd'
-  regpt_dt = 'RegPT/pk_RegPT.dat_dt'
-  regpt_tt = 'RegPT/pk_RegPT.dat_tt'
+  call Ini_Open(InputFile, 1, bad, .false.)
 
-  outroot = 'output/test'
-  
-  use_nonlinear = .true.
+  pkfile = Ini_Read_String('pkfile')
+  outroot = Ini_Read_String('outroot')
 
-  ! Approx z=0 values 
-  !ff = 0.492	
-  !sigmav = 6.07
-   ! If setting sigma ne 0.0 P(k) is rescaled to give specified value
-  !sigma_8 = 0.817
+  use_nonlinear = Ini_Read_Logical('use_nonlinear',.false.)
 
-  ! Approx z=0.57 values 
-  sigmav = 4.44
-  !ff = 0.785
-  ff = 0.68
+  if (use_nonlinear) then
+    regpt_dd = Ini_Read_String('regpt_dd')
+    regpt_dt = Ini_Read_String('regpt_dt')
+    regpt_tt = Ini_Read_String('regpt_tt')
+  else
+    regpt_dd = ''
+    regpt_dt = ''
+    regpt_tt = ''
+  endif
 
-  ! Bias terms 
-  b1 = 2.035
-  !b2 = 2.8
-  b2 = 2.5
+  ff = Ini_Read_Double('ff')	
+  sigmav = Ini_Read_Double('sigmav')
 
-  !bs2 = 1.0
-
+  b1 = Ini_Read_Double('b1')
+  b2 = Ini_Read_Double('b2')
+  bs2 = Ini_Read_Double('bs2',0.0d0)
+ 
   write(*,*) 'P_0/P_2 = ', (1.0+2.0/3.0*ff/b1+1.0/5.0*(ff/b1)**2)/(4.0/3.0*ff/b1+4.0/7.0*(ff/b1)**2)
 
   call load_matterpower_data(pkfile,regpt_dd,regpt_dt,regpt_tt) 
